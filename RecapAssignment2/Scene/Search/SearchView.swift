fb54798ed6a9fff42b545294af741d726bf309e4
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-09-07 20:42:32 +0900

    searchViewController 파일 추가 및 SceneDelegate 루트 뷰 설정

---
+++ RecapAssignment2/Scene/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  RecapAssignment2
+//
+//  Created by 김지연 on 2023/09/07.
+//
+
+import UIKit
+
+class SearchView: BaseView {
+    
+    override func configure() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+}
---
+++ RecapAssignment2/Scene/Search/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  RecapAssignment2
+//
+//  Created by 김지연 on 2023/09/07.
+//
+
+import UIKit
+
+class SearchViewController: BaseViewController {
+    
+    let mainView = SearchView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = mainView
+    }
+    
+    override func configure() {
+        super.configure()
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    
+    
+}
